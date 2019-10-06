56b11fadf895c6b37b5fdaf6f5ad2893627b9280
Sahil Verma <iamsahil1910@gmail.com>
2019-10-06 17:12:57 +0530

    Updates

---
+++ precache-manifest.bce17bcb1cd9dea68d8b2c33c22d0109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648d1514eadf57d4b00e7b0b72b3f52e",
+    "url": "/Videos/index.html"
+  },
+  {
+    "revision": "b14c04530e1ff0f2271e",
+    "url": "/Videos/static/css/main.bb8073e6.chunk.css"
+  },
+  {
+    "revision": "428286251fd62ce24e5a",
+    "url": "/Videos/static/js/2.9c6290ff.chunk.js"
+  },
+  {
+    "revision": "b14c04530e1ff0f2271e",
+    "url": "/Videos/static/js/main.a08396f1.chunk.js"
+  },
+  {
+    "revision": "792a5d7a5eeed77e2325",
+    "url": "/Videos/static/js/runtime-main.fa958d39.js"
+  }
+]);
